a28c57500511151353c7658e85fd6b8d3db7dccb
Michael Wenz <wenzm9@gmail.com>
2019-04-03 23:41:25 +0800

    added heroku info

--- polls_project/settings.py
+++ polls_project/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'polls_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+#
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "polls2019",
+        "USER": "Wenz",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
