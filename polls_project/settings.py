92f3f1451e42f8e2d0a5b53b9b5ed9d2f958a5ac
Michael Wenz <wenzm9@gmail.com>
2019-04-04 11:07:32 +0800

    removed import file

--- polls_project/settings.py
+++ polls_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-from secrets import SECRET_KEY
+
 import os
 import django_heroku
 
