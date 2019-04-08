28d6894f52a3f4261eca5492f4102e7edbbf3b5e
Michael Wenz <wenzm9@gmail.com>
2019-04-08 13:34:59 +0800

    before aws delployment

--- polls_project/settings.py
+++ polls_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,11 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-## This is used for running it locally
-# SECRET_KEY = SECRET_KEY
+# This is used for running it locally
+from secrets import SECRET_KEY
+SECRET_KEY = SECRET_KEY
 
-## this is for Heroku
-SECRET_KEY = os.environ['SECRET_KEY']
+# ## this is for Heroku
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -139,6 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
+# # this is for Heroku only
+# django_heroku.settings(locals())
 
